22787b9bfe56fe0ac2af4f128cf94809bce36e78
Teik Tee <teikkeat80@gmail.com>
2023-04-29 16:32:02 +0100

    Create CreateDbAndTable.sql

---
+++ Snowpipe/CreateDbAndTable.sql
@@ -0,0 +1,21 @@
+-- Create Snowflake database for the project
+CREATE OR REPLACE DATABASE FIN_PROJ;
+
+-- Set DATABASE and SCHEMA
+USE DATABASE FIN_PROJ;
+USE SCHEMA PUBLIC;
+
+-- Create original table
+CREATE OR REPLACE TABLE snp500_price (
+"date" date,
+open float,
+high float,
+low float,
+close float,
+adjusted_close float,
+volume integer,
+dividend_amount double,
+split_coefficient double,
+symbol string,
+primary key ("date", symbol)
+);
